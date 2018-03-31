53ff45d07af0050dac9bfb58293f052bc03ac316
Konrad Wisłocki <wislockik23@gmail.com>
2018-03-31 18:01:58 +0200

    added mobile navigation(with html structure refactoring) and some fixes

--- js/main.js
+++ js/main.js
@@ -2,20 +2,22 @@
 
     $(document).ready(function() {
         var $nav = $('.navigation');
-        var navOffsetY = $nav.offset().top;
+        //var navOffsetY = $nav.offset().top;
         var $innerAnchors = $('a[href^="#"]');
         var stickyNav = function() {
             var scrolled = $(document).scrollTop();
-            if (scrolled > navOffsetY) $nav.addClass('navigation--sticky');
+            if (scrolled > 0) $nav.addClass('navigation--sticky');
             else $nav.removeClass('navigation--sticky');
         }
 
         if ($innerAnchors.length > 0) {
             $innerAnchors.on('click', function(e) {
                 e.preventDefault();
+                $nav.removeClass('navigation--mobile');
+                $('.navigation__toggle').removeClass('navigation__toggle--active');
                 const href= $(this).attr('href');
                 const $targetElement = $(href); 
-                const targetToScroll = $targetElement.offset().top - $nav.height();
+                const targetToScroll = $targetElement.offset().top;
                 $('html, body').animate({
                     scrollTop:targetToScroll
                     }, 1000);
@@ -24,6 +26,7 @@
 
             $('.navigation__toggle').on('click', function() {
                 $(this).toggleClass('navigation__toggle--active');
+                $nav.toggleClass('navigation--mobile');
             });
 
         stickyNav();
